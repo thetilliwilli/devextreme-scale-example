a702b331ed54d6b478fb426ab518db33c3a961b4
TilliWilli <tilliwilli@gmail.com>
2019-06-10 22:46:14 +0300

    +dataGrid

---
+++ source/data-grid-instance.js
@@ -0,0 +1,28 @@
+const dxDataGrid = require("devextreme/ui/data_grid")
+const { generateData } = require("./data-grid-generate-data");
+
+const dataGridOptions = {
+    dataSource: generateData(100000),
+    showBorders: true,
+    customizeColumns: function (columns) {
+        columns[0].width = 70;
+    },
+    loadPanel: {
+        enabled: true
+    },
+    scrolling: {
+        mode: "virtual"
+    },
+    sorting: {
+        mode: "none"
+    },
+    onContentReady: function (e) {
+        e.component.option("loadPanel.enabled", false);
+    }
+};
+
+var dataGrid = new dxDataGrid("#sales", dataGridOptions);
+
+module.exports = {
+    dataGrid,
+};
